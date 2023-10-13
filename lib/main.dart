7302c0bc91902293db181111b61db338356c4a4d
gontijol <gontijol@live.com>
2023-10-12 22:29:14 -0300

    Correção na main

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,17 @@ import 'package:clima_ufg/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load();
-  runApp(
-    GetMaterialApp(
-      title: 'Clima UFG',
-      initialRoute: '/',
-      debugShowCheckedModeBanner: false,
-      getPages: AppRoutes.pages,
-      initialBinding: HomeBinding(),
-    ),
-  );
+  runApp(GetMaterialApp(
+    title: 'Clima UFG',
+    initialRoute: '/',
+    debugShowCheckedModeBanner: false,
+    getPages: AppRoutes.pages,
+    initialBinding: HomeBinding(),
+  ));
 }
